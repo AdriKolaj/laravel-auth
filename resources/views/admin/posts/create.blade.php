35f9c5bba3f93d4f57c34555da9908d6ac817e3e
adri <adri.kolaj@gmail.com>
2021-02-25 17:21:06 +0100

    Primo commit

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.app')
+
+@section('content')
+
+  <div class="container">
+    <h1>Crea un nuovo Post</h1>
+
+    @if ($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+          @foreach ($errors->all() as $error)
+              <li>{{ $error }}</li>
+          @endforeach
+        </ul>
+      </div>
+    @endif
+
+    <form action="{{ route('admin.posts.store') }}" method="POST">
+      @csrf
+      @method('POST')
+
+      <div class="form-group">
+        <label for="title">Titolo</label>
+        <input class="form-control" type="text" id="title" name="title" value="{{ old('title') }}">
+      </div>
+
+      <div class="form-group">
+        <label for="author">Autore</label>
+        <input class="form-control" type="text" id="author" name="author" value="{{ old('author') }}">
+      </div>
+
+      <div class="form-group">
+        <label for="body">Testo</label>
+        <textarea class="form-control" name="body" id="body" rows="10">{{ old('body') }}</textarea>
+      </div>
+
+      <input class="btn btn-primary" type="submit" value="Salva"> 
+    </form>
+  </div>
+
+@endsection
---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.app')
+
+@section('content')
+
+  <div class="container">
+    <h1>Tutti i post</h1>
+    <table class="table table-striped table-bordered">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Titolo</th>
+          <th>Autore</th>
+          <th>Data creazione</th>
+          <th colspan="3"></th>
+        </tr>
+      </thead>
+
+      <tbody>
+        @foreach ($posts as $post)
+            <tr>
+              <td>{{ $post->id }}</td>
+              <td>{{ $post->title }}</td>
+              <td>{{ $post->author }}</td>
+              <td>{{ $post->created_at->format('d-m-Y') }}</td>
+            </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+
+@endsection
